464b324ac65479120e18bbf32b6519f6d2b74adc
thecatblake <k222ryousuke@gmail.com>
2023-11-08 07:21:53 +0900

    update sidewinder_maze.cpp

--- sidewinder_maze.cpp
+++ sidewinder_maze.cpp
@@ -11,5 +11,9 @@ int main() {
     Grid grid(30, 30);
     binary_tree(grid);
 
+    char* file_name = "sidewinder_maze.png";
+
+    grid.toPng(file_name, 10);
+
     std::cout << grid.toString() << std::endl;
 }
